8aac07b55cea8345e00bc834d82f94e2368e772c
Ne2109 <79627860+Ne2109@users.noreply.github.com>
2021-05-15 11:56:46 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-function preload()
-{
+noseX = 0;
+noseY = 0;
+
+function preload(){
+  clown_nose = loadImage('https://i.postimg.cc/MH4Fhfmk/Clown-nose-large.png');
 }
 
 function setup()
@@ -19,15 +22,20 @@ function modelLoaded(){
     console.log('PoseNet Is Initialized');
 }
  
-function draw()
-{
+function draw(){
+    fill (255,0,0);
+    stroke (255,0,0);
+    circle(noseX , noseY , 20);
     image(video,0,0,300,300);
+   // image(clown_nose, noseX, noseY, 30 , 30 ); 
 }
 
 function gotPoses(results)
 {
     if(results.length>0)
     console.log(results);
+    noseX = results[0].pose.nose.x;
+    noseY = results[0].pose.nose.y;
     console.log("nose x = "+ results[0].pose.nose.x);
     console.log("nose y = "+ results[0].pose.nose.y);
 
